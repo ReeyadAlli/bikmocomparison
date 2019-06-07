1a92db47d50370fc366cf22c1c84cee99add35fa
Reeyad Alli <reeyad_10@hotmail.com>
2019-06-07 02:47:57 +0100

    Task # @00h:30m - Update layout and css for navbar and page template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bikmo-comparison';
   comparisonLinks = [
-    { path: 'service-comparison', label: 'Service' },
-    { path: 'parts-comparison', label: 'Parts' },
-    { path: 'new-comparison', label: 'New Bike' }
+    { path: 'service-comparison', label: 'Service', icon: 'autorenew'},
+    { path: 'parts-comparison', label: 'Parts', icon: 'settings'},
+    { path: 'new-bike-comparison', label: 'New Bike', icon: 'motorcycle'}
   ];
 }
