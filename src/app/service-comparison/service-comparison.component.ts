f5f496ff321bca7f88ece20158f2d27c794647b0
Reeyad Alli <reeyad_10@hotmail.com>
2019-06-07 04:28:30 +0100

    Task # @01h:00m - Add bike service data and styled list to service comparison list.

--- src/app/service-comparison/service-comparison.component.ts
+++ src/app/service-comparison/service-comparison.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BikeServiceModel } from '../Models/BikeService';
+import { Provider } from '../Models/Provider';
 @Component({
   selector: 'app-service-comparison',
   templateUrl: './service-comparison.component.html',
   styleUrls: ['./service-comparison.component.css']
 })
 export class ServiceComparisonComponent implements OnInit {
+  loading = true;
+  availableServices: BikeServiceModel[];
 
-  constructor() { }
+  constructor() {
+    const evans  = new Provider('Evans', 'https://www.thehive-network.com/wp-content/uploads/2017/07/logo-EvansCycles-full.jpg');
+    const halfords  = new Provider('Halfords', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/36/Halfords_logo.svg/241px-Halfords_logo.svg.png');
+
+    this.availableServices = [
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', evans),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', evans),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', evans),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+      new BikeServiceModel('Bronze - Entry Level', 'Gold', '£100', 'Entry level service', halfords),
+    ];
+   }
 
   ngOnInit() {
+    setTimeout((_ = this) => {_.loading = false; }, 30000);
   }
 
 }
